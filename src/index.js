4053f7104daeac8aa9b72abedf68ce7f53676b4d
Joshua Napoli <josh@cumulusds.com>
2023-09-17 12:29:03 -0400

    Factor out renderVulnerableRepositoryToConsole function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 // @flow
 
-import chalk from "chalk";
-import { label } from "./repository";
 import generateVulnerableRepositories from "./generateVulnerableRepositories";
-import getAdvisories from "./getAdvisories";
 import generateOrganizationRepositories from "./generateOrganizationRepositories";
+import renderVulnerableRepositoryToConsole from "./renderVulnerableRepositoryToConsole";
 
 export default async function main() {
   for await (const repository of generateVulnerableRepositories(generateOrganizationRepositories())) {
-    const { name } = repository;
-    const advisories = getAdvisories(repository);
-    console.log(chalk`{bold ${name}}`);
-    advisories.forEach(({ ghsaId, severity, summary }) => console.log(`\t${ghsaId} ${label[severity]} ${summary}`));
-    console.log();
+    renderVulnerableRepositoryToConsole(repository);
   }
 }
