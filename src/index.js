c19664d7db67eff8b7fed4c769fb5cffc041f734
Joshua Napoli <josh@cumulusds.com>
2023-09-17 12:23:10 -0400

    Factor out generateOrganizationRepositories function

--- src/index.js
+++ src/index.js
@@ -1,64 +1,10 @@
 // @flow
 
 import chalk from "chalk";
-import { graphql } from "@octokit/graphql";
-import type { Repository } from "./repository";
 import { label } from "./repository";
 import generateVulnerableRepositories from "./generateVulnerableRepositories";
 import getAdvisories from "./getAdvisories";
-
-function github() {
-  return process.env.GITHUB_TOKEN == null
-    ? graphql
-    : graphql.defaults({
-        headers: {
-          authorization: `token ${process.env.GITHUB_TOKEN}`
-        }
-      });
-}
-
-async function getOrganizationRepositories(after) {
-  return github()(
-    `
-query OrganizationRepositories($after: String) {
- organization(login: "cumulusds") {
-    repositories(first: 100, after: $after) {
-      pageInfo {
-        endCursor,
-        hasNextPage
-      }
-      nodes {
-        name,
-        vulnerabilityAlerts(first: 100) {
-          nodes {
-            dismissedAt,
-            autoDismissedAt,
-            fixedAt,
-            securityVulnerability {
-              advisory { ghsaId, summary },
-              severity
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`,
-    { after }
-  );
-}
-
-async function* generateOrganizationRepositories(): AsyncIterator<Repository> {
-  const firstPage = await getOrganizationRepositories();
-  yield* firstPage.organization.repositories.nodes;
-  let { pageInfo } = firstPage.organization.repositories;
-  while (pageInfo.hasNextPage) {
-    const page = await getOrganizationRepositories(pageInfo.endCursor);
-    yield* page.organization.repositories.nodes;
-    pageInfo = page.organization.repositories.pageInfo;
-  }
-}
+import generateOrganizationRepositories from "./generateOrganizationRepositories";
 
 export default async function main() {
   for await (const repository of generateVulnerableRepositories(generateOrganizationRepositories())) {
