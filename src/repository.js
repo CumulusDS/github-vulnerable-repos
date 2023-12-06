619c405c6fa627538a7f9245cad824e96f2e9080
Joshua Napoli <josh@cumulusds.com>
2023-12-05 23:08:16 -0500

    Write a report pdf

--- src/repository.js
+++ src/repository.js
@@ -10,6 +10,20 @@ export const label: {| CRITICAL: string, HIGH: string, LOW: string, MODERATE: st
   LOW: chalk`{cyan      LOW}`
 };
 
+export const labelText: {| CRITICAL: string, HIGH: string, LOW: string, MODERATE: string |} = {
+  CRITICAL: ` CRITICAL`,
+  HIGH: `     HIGH`,
+  MODERATE: ` MODERATE`,
+  LOW: `      LOW`
+};
+
+export const labelColor: {| CRITICAL: string, HIGH: string, LOW: string, MODERATE: string |} = {
+  CRITICAL: `red`,
+  HIGH: `magenta`,
+  MODERATE: `green`,
+  LOW: `cyan`
+};
+
 export type Severity = $Keys<typeof label>;
 
 // See https://docs.github.com/en/graphql/reference/objects#securityadvisoryidentifier
